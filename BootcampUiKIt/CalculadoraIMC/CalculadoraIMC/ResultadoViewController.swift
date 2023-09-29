53d60435e439634415192a7ca10afd8364593a50
Alan Caro <alan.caro.m@icloud.com>
2023-09-29 17:48:40 -0600

    Act 29-sep-23

---
+++ BootcampUiKIt/CalculadoraIMC/CalculadoraIMC/ResultadoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ResultadoViewController.swift
+//  CalculadoraIMC
+//
+//  Created by HEBER JIMENEZ on 29/09/23.
+//
+
+import UIKit
+
+class ResultadoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ BootcampUiKIt/CalculadoraIMC/CalculadoraIMC/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CalculadoraIMC
+//
+// https://www.youtube.com/watch?v=OPwsvkunP2A&ab_channel=MarcoAlonso
+// minuto 1:20:00
+// 2:35:03
+//
+//  Created by HEBER JIMENEZ on 29/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
